1d7c305f9c401a42922980b77f33d29f34ae91a8
bsr <bsr@DESKTOP-GTR4O03>
2024-07-26 12:21:22 +0200

    + 1.1.15

---
+++ dist/esm/tab/utils.d.ts
@@ -0,0 +1,5 @@
+import React, { ReactElement } from "react";
+export declare function openItem(id: string, prefix: string, callback?: () => void): void;
+export declare function getButtonContent(icon: any, name?: string | ReactElement): React.JSX.Element;
+export declare function setShow(id: string, prefix: string, value: boolean): void;
+export declare function setDisabled(id: string, prefix: string, value: boolean): void;
---
+++ dist/index.d.ts
@@ -0,0 +1,40 @@
+import React, { ReactElement, Component } from 'react';
+
+type TabProps = {
+    children: React.ReactNode;
+    buttonPrefix?: string;
+    onSelect?: (eventKey?: string, id?: string) => void;
+};
+type ItemTabProps = {
+    eventKey?: string;
+    title?: string | ReactElement;
+    icon?: ReactElement;
+    isOpen?: boolean;
+    children: React.ReactNode;
+    id?: string;
+    _prefix?: string;
+    width?: number;
+};
+
+declare class Tab extends Component<ItemTabProps, any> {
+    constructor({ props }: {
+        props: Readonly<ItemTabProps>;
+    });
+    SelectTab(callback?: () => void): void;
+    SetShow(value: boolean): void;
+    SetDisabled(value: boolean): void;
+    render(): React.JSX.Element[] | null | undefined;
+}
+
+declare class Tabs extends Component<TabProps, any> {
+    private list;
+    constructor({ props }: {
+        props: Readonly<TabProps>;
+    });
+    SelectTabsById(id: string, callback?: () => void): void;
+    innerRender(): void;
+    innerOpenTab(id: string, prefix: string, eventKey?: string): void;
+    render(): React.JSX.Element;
+}
+
+export { type ItemTabProps, Tab, type TabProps, Tabs };
